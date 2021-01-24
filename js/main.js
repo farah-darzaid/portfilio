6001c031ac6c31c3fe16fe1b9a4bdc35ce269d6d
farah <account@peak.jo>
2021-01-24 16:44:37 +0200

    final Edit

--- js/main.js
+++ js/main.js
@@ -3,4 +3,20 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY> 0);
 
-})
+});
+
+
+let tog = document.getElementById('toggle');
+
+document.onclick = toggleMenu;
+ function toggleMenu() {
+    var menuToggle = document.querySelector(".toggle");
+    var menu = document.querySelector(".menu");
+
+    menuToggle.classList.toggle("active")
+    menu.classList.toggle("active")
+
+}
+
+
+
